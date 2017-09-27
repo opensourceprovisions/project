872f8b2917e4f7d545ad2e8fc63b662c4827c401
root <root@ip-172-31-24-112.us-west-2.compute.internal>
2017-09-27 18:04:33 +0000

    Initial commit

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+// define database connection
+  define('DB_SERVER', 'localhost'); // eg, localhost - should not be empty for productive servers
+  define('DB_SERVER_USERNAME', 'root');
+  define('DB_SERVER_PASSWORD', 'Indigo#2017');
+  define('DB_DATABASE', 'rukovoditel');
+  
+  
